b7e65fee6598a05e4cfc277e8405b684abb11d9c
crx <369383829@qq.com>
2020-04-27 21:09:10 +0800

    6-3_编码实战：实现初始化分块上传接口;6-4_编码实战：实现分块上传接口;6-5_编码实战：实现分块合并接口

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module filestore-server
 go 1.14
 
 require (
-	github.com/garyburd/redigo v1.6.0 // indirect
+	github.com/garyburd/redigo v1.6.0
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/json-iterator/go v1.1.9 // indirect
 )
--- main.go
+++ main.go
@@ -16,13 +16,18 @@ func main(){
 	http.HandleFunc("/file/query",handle.FileQueryHandler)
 	http.HandleFunc("/file/fastupload",handle.TryFastUploadHandle)
 
+	// 分块上传接口
+	http.HandleFunc("/file/mpupload/init",handle.HTTPInterceptor(handle.InitialMultipartUploadHandler))
+	http.HandleFunc("/file/mpupload/uppart",handle.HTTPInterceptor(handle.UploadPartHandle))
+	http.HandleFunc("/file/mpupload/complete",handle.HTTPInterceptor(handle.CompleteUploadHandler))
+
 	http.HandleFunc("/user/signup",handle.SignupHandler)
 	http.HandleFunc("/user/signin",handle.SignInHandler)
 	http.HandleFunc("/user/info",handle.HTTPInterceptor(handle.UserInfoHandler))
 
 	http.Handle("/static/",
 		http.StripPrefix("/static/",http.FileServer(http.Dir("./static"))))
-	err := http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil{
 		fmt.Printf("Failed to start , err:%s",err.Error())
 	}
